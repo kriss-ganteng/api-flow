56d685597f4e3ba69386018bd87e07a6ded793d3
kriss <kriss@asop.klik.digital>
2019-10-18 11:32:46 +0700

    api-test

---
+++ Test Suites/testapi.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testapi</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>42399be8-cf93-42f7-80a7-23d8e1712e6a</testSuiteGuid>
+   <testCaseLink>
+      <guid>9c5f8a57-f73b-49fe-b041-f1d6227f9517</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/api-test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
